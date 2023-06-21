c01b3265d2e28e6a30d71dd01508586b08d16cfd
rh5661 <rh5661@rit.edu>
2023-06-21 13:07:36 -0400

    implemented account data retrieval - "Request Monthly Usage"

--- src/PPLTool.java
+++ src/PPLTool.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PPLTool {
 
@@ -20,6 +21,38 @@ public class PPLTool {
         userNameInput.sendKeys(username);
         passwordInput.sendKeys(password);
         loginButton.click();
+    }
+
+    public static void retrieveAccountData(WebDriver driver, String accountNumber) {
+        /*
+        * Retrieves Monthly Usage of an Account Number
+        */
+        // redirect to "Request Monthly Usage" webpage
+        driver.get("https://supplier.prod.pplweb.com/eusupplierportal/Secured/Retail/RequestMonthlyUsage.aspx");
+
+        WebElement accountInput = driver.findElement(By.id("MainContent_macct_txtKyBa"));
+        WebElement submitAccount = driver.findElement(By.id("MainContent_macct_btnAddAccount"));
+
+        // enter account# and add account
+        accountInput.sendKeys(accountNumber);
+        submitAccount.click();
+
+        // get usage
+        WebElement getUsage = driver.findElement(By.id("MainContent_btnGetUsage"));
+        getUsage.click();
+
+    }
+
+    public static void main(String[] args) {
+        String loginUsername = "CBergerCE1";
+        String loginPassword = "ces10038!";
+
+        String accountNumber = "2087098013";
+
+        ChromeDriver driver = new ChromeDriver();
+
+        loginAccount(driver, loginUsername, loginPassword);
+        retrieveAccountData(driver, accountNumber);
     }
 
 }
