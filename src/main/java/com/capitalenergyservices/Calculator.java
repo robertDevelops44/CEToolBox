5e43166912969a840a6633844817dc36fa2557c2
rh5661 <rh5661@rit.edu>
2023-07-17 15:40:50 -0400

    margin/broker fee calculator

--- src/main/java/com/capitalenergyservices/Calculator.java
+++ src/main/java/com/capitalenergyservices/Calculator.java
@@ -1,3 +1,12 @@
 public class Calculator {
 
+    public static double calculateFee(double ourRate, double supplierRate, double utilityTaxPercentage) {
+        /*
+        * calculates the broker fee/margin given
+        * ourRate - rate providing
+        * supplierRate - rate given by supplier (w/tax)
+        * utilityTaxPercentage - the utility tax percentage of the state */
+        return (ourRate - supplierRate) * ((100-utilityTax)/100)
+    }
+
 }
